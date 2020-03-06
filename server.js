c48794657dee696d6908dd2b866d4c9516cf6a84
divasilevski <divasilevski@gmail.com>
2020-03-06 13:50:08 +0300

    added socket

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const serveStatic = require("serve-static");
 const path = require("path");
+const socketIO = require('socket.io');
 
 const app = express();
 
@@ -10,3 +11,12 @@ const port = process.env.PORT || 8080;
 app.listen(port);
 
 console.log(`Listening on port: ${port}`);
+
+const io = socketIO(app);
+
+io.on('connection', (socket) => {
+    console.log('Client connected');
+    socket.on('disconnect', () => console.log('Client disconnected'));
+  });
+  
+  setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
