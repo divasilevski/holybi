35c2b6822caeeef9982b229f364b6dae3cc2b46e
divasilevski <divasilevski@gmail.com>
2020-03-06 18:19:00 +0300

    create input part

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,39 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-container class="fill-height justify-center">
+    <v-form ref="form" v-model="valid" lazy-validation @submit.prevent>
+      <v-text-field
+        v-model="room_id"
+        class="ma-5"
+        label="Комната"
+        :rules="rules"
+        hide-details="auto"
+        v-on:keyup.enter="clickEnter"
+        required
+      ></v-text-field>
+
+      <v-btn class="ma-5" @click="clickEnter">Войти в комнату</v-btn>
+    </v-form>
+  </v-container>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "Home",
+  data: () => ({
+    valid: true,
+    room_id: "",
+    rules: [
+      v => !!v || "Придумайте id для комнаты",
+      v => (v && v.length <= 10) || "id превышет 10 символов"
+    ]
+  }),
+  methods: {
+    clickEnter() {
+      if (this.$refs.form.validate()) {
+        console.log(`Вход в комнату ${this.room_id}`);
+      }
+    }
+  },
+  components: {}
+};
 </script>
