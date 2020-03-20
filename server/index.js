273ab1e80bf4c76dfdd121d44c8adb8f7aa97bfd
divasilevski <divasilevski@gmail.com>
2020-03-20 13:04:16 +0300

    save project for other peple

--- server/index.js
+++ server/index.js
@@ -10,6 +10,12 @@ const users = require("./users")();
 io.on("connection", socket => {
   //
   socket.on("UPDATE_PROJECT", ({ project, last, room }) => {
+    const room_users = users.getByRoom(room);
+
+    if (room_users[0]) {
+      room_users[0].project = project;
+      room_users[0].last = last;
+    }
     socket.to(room).broadcast.emit("UPDATE_PROJECT", { project, last });
   });
 
@@ -25,14 +31,19 @@ io.on("connection", socket => {
     socket.join(room);
     io.to(room).emit("UPDATE_USERS", room_users);
 
+
     room_users[0].messages
-      ? room_users[0].messages.push({
-          type: "admin",
-          message: `Пользователь ${user.name} присоединился!`
-        })
-      : (room_users[0].messages = [
-          { type: "admin", message: `Да здравствует, король ${user.name}!` }
-        ]);
+        ? room_users[0].messages.push({
+            type: "admin",
+            message: `Пользователь ${user.name} присоединился!`
+          })
+        : (room_users[0].messages = [
+            { type: "admin", message: `Да здравствует, король ${user.name}!` }
+          ]);
+
+    if (room_users[0].project) {
+      socket.emit("UPDATE_PROJECT", { project: room_users[0].project, last: room_users[0].last });
+    }
 
     io.to(room).emit("UPDATE_MESSAGES", room_users[0].messages);
   });
@@ -91,6 +102,8 @@ io.on("connection", socket => {
         if (user.king) {
           room_users[0].king = true;
           room_users[0].messages = user.messages;
+          room_users[0].project = user.project;
+          room_users[0].last = user.last;
           io.to(room_users[0].id).emit("SET_USER", room_users[0]);
 
           room_users[0].messages.push({
